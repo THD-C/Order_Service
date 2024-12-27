a8da878c8c06bbb8cb0ea02f93e1c1196efd5f7c
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-12-27 16:45:39 +0100

    Add logging interceptor

---
+++ internal/interceptor/logger_interceptor.go
@@ -0,0 +1,44 @@
+package interceptor
+
+import (
+	"context"
+	"github.com/rs/zerolog"
+	"google.golang.org/grpc"
+	"time"
+)
+
+func LoggerInterceptor(logger zerolog.Logger) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		startTime := time.Now()
+		logger.Info().
+			Str("method", info.FullMethod).
+			Interface("request", req).
+			Msg("Incoming gRPC request")
+
+		resp, err := handler(ctx, req)
+
+		duration := time.Since(startTime)
+		if err != nil {
+			logger.Error().
+				Str("method", info.FullMethod).
+				Interface("request", req).
+				Err(err).
+				Dur("duration", duration).
+				Msg("gRPC request failed")
+		} else {
+			logger.Info().
+				Str("method", info.FullMethod).
+				Interface("request", req).
+				Interface("response", resp).
+				Dur("duration", duration).
+				Msg("gRPC request succeeded")
+		}
+
+		return resp, err
+	}
+}
