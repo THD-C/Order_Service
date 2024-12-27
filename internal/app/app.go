018fa5a60ae5a90ac5ac1e6607d8f8b1e2fa0d09
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-12-27 17:10:46 +0100

    Use new config fields

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,32 +1,49 @@
 package app
 
 import (
-	"context"
-	"github.com/rs/zerolog/log"
+	"fmt"
+	"github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"net"
+	"net/http"
 	"order_service/generated/order"
 	"order_service/generated/wallet"
 	"order_service/internal/cache"
 	"order_service/internal/config"
+	"order_service/internal/interceptor"
+	"order_service/internal/logger"
 	"order_service/internal/server"
 )
 
+func startPrometheusMetrics() {
+	http.Handle("/metrics", promhttp.Handler())
+	_ = http.ListenAndServe(":8080", nil)
+}
+
 func startGRPCServer() {
-	lis, err := net.Listen("tcp", ":50051")
+	log := logger.GetLogger()
+
+	conf := config.GetConfig()
+	addr := fmt.Sprintf("%s:%s", conf.Addr, conf.Port)
+
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatal().Msgf("failed to listen: %v", err)
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	priceCache := cache.NewPriceCache()
-	go priceCache.UpdatePrices(ctx, config.GetConfig().CoingeckoPollingFrequency)
+	grpcMetrics := grpc_prometheus.NewServerMetrics()
+	opts := []grpc.ServerOption{
+		grpc.ChainUnaryInterceptor(
+			grpcMetrics.UnaryServerInterceptor(),
+			interceptor.UnaryInterceptor(logger.GetLogger()),
+		),
+	}
 
-	s := grpc.NewServer()
+	s := grpc.NewServer(opts...)
 	wallet.RegisterWalletsServer(s, &server.WalletServer{})
-	order.RegisterOrderServer(s, server.NewOrderServer(priceCache))
+	order.RegisterOrderServer(s, server.NewOrderServer())
 	reflection.Register(s)
 
 	log.Printf("server listening at %v", lis.Addr())
@@ -36,6 +53,9 @@ func startGRPCServer() {
 }
 
 func App() {
+	logger.Init()
+	log := logger.GetLogger()
+
 	_, err := config.LoadConfig()
 	if err != nil {
 		log.Fatal().Msg("Failed to read config from file")
@@ -45,5 +65,6 @@ func App() {
 	if err != nil {
 		log.Fatal().Msg("Failed to fetch wallets")
 	}
+	go startPrometheusMetrics()
 	startGRPCServer()
 }
