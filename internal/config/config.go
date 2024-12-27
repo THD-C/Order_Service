b2a8ee0ee758867fb93cc4f1d89a875079ceaa9c
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-12-27 17:06:38 +0100

    Add addres and port to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,6 +8,8 @@ import (
 )
 
 type Config struct {
+	Addr                      string
+	Port                      string
 	DBManagerAddress          string
 	CoingeckoServiceAddress   string
 	DBManagerTimeout          time.Duration
@@ -24,20 +26,34 @@ func LoadConfig() (*Config, error) {
 	var err error
 	once.Do(
 		func() {
+			applicationAddr := os.Getenv("APPLICATION_ADDR")
+			if applicationAddr == "" {
+				applicationAddr = "localhost"
+			}
+
+			applicationPort := os.Getenv("APPLICATION_PORT")
+			if applicationPort == "" {
+				applicationPort = "50051"
+			}
+
 			dbManagerTimeout, err := strconv.Atoi(os.Getenv("DB_MANAGER_TIMEOUT"))
 			if err != nil {
 				dbManagerTimeout = 30
 			}
+
 			coingeckoServiceTimeout, err := strconv.Atoi(os.Getenv("COINGECKO_SERVICE_TIMEOUT"))
 			if err != nil {
 				coingeckoServiceTimeout = 30
 			}
+
 			coingeckoPollingFrequency, err := strconv.Atoi(os.Getenv("COINGECKO_POLLING_FREQUENCY"))
 			if err != nil {
 				coingeckoPollingFrequency = 60
 			}
 
 			instance = &Config{
+				Addr:                      applicationAddr,
+				Port:                      applicationPort,
 				DBManagerAddress:          os.Getenv("DB_MANAGER_ADDRESS"),
 				CoingeckoServiceAddress:   os.Getenv("COINGECKO_SERVICE_ADDRESS"),
 				DBManagerTimeout:          time.Duration(dbManagerTimeout) * time.Second,
