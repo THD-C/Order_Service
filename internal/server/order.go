a7deb7d0a0ac403dcec42f4f761559f5c531cc37
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-12-27 17:20:40 +0100

    Remove log

--- internal/server/order.go
+++ internal/server/order.go
@@ -3,7 +3,6 @@ package server
 import (
 	"context"
 	"fmt"
-	"github.com/rs/zerolog/log"
 	proto "order_service/generated/order"
 	"order_service/internal/service/order_service"
 )
@@ -29,14 +28,12 @@ func (s *OrderServer) CreateOrder(
 	ctx context.Context,
 	req *proto.OrderDetails,
 ) (*proto.OrderDetails, error) {
-	log.Info().Interface("request", req).Msg("Creating order")
 	switch req.Side {
 	case proto.OrderSide_ORDER_SIDE_BUY:
 		return s.buyOrderService.CreateOrder(ctx, req)
 	case proto.OrderSide_ORDER_SIDE_SELL:
 		return s.sellOrderService.CreateOrder(ctx, req)
 	default:
-		log.Error().Interface("request", req).Msg(UnsupportedOrderSide)
 		return nil, fmt.Errorf(UnsupportedOrderSide)
 	}
 }
