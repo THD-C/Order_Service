e9eb090366dbd5b1278fd52c22b8a3b5ca186eae
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-12-27 17:25:52 +0100

    Add coin price struct

--- internal/types/coin_price.go
+++ internal/types/coin_price.go
@@ -1,51 +1,25 @@
 package types
 
 import (
-	"fmt"
 	"github.com/shopspring/decimal"
-	// proto "order_service/generated/coins"
+	"sync"
 )
 
-type Coin struct {
-	ID   string          `json:"id"`
-	Buy  decimal.Decimal `json:"buy"`
-	Sell decimal.Decimal `json:"sell"`
+type CoinPrice struct {
+	CoinId string
+	Prices map[string]decimal.Decimal
+	mu     sync.Mutex
 }
 
-func NewCoin(id, buy, sell string) *Coin {
-	buyVal, _ := decimal.NewFromString(buy)
-	sellVal, _ := decimal.NewFromString(sell)
-
-	return &Coin{
-		ID:   id,
-		Buy:  buyVal,
-		Sell: sellVal,
+func NewCoinPrice(coinId string) *CoinPrice {
+	return &CoinPrice{
+		CoinId: coinId,
+		Prices: make(map[string]decimal.Decimal),
 	}
 }
 
-func (c *Coin) String() string {
-	return fmt.Sprintf(
-		"[Coin ID: %s, Buy: %s, Sell: %s]",
-		c.ID,
-		c.Buy,
-		c.Sell,
-	)
+func (cp *CoinPrice) SetPrices(prices map[string]decimal.Decimal) {
+	cp.mu.Lock()
+	defer cp.mu.Unlock()
+	cp.Prices = prices
 }
-
-// func (c *Coin) FromProto(p *proto.Coin) error {
-// 	buy, err := decimal.NewFromString(p.Buy)
-// 	if err != nil || buy.LessThan(decimal.Zero) {
-// 		return fmt.Errorf("wrong buy value")
-// 	}
-//
-// 	sell, err := decimal.NewFromString(p.Sell)
-// 	if err != nil || sell.LessThan(decimal.Zero) {
-// 		return fmt.Errorf("wrong sell value")
-// 	}
-//
-// 	c.ID = p.Id
-// 	c.Buy = buy
-// 	c.Sell = sell
-//
-// 	return nil
-// }
