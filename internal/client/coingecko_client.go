f4dcae16a256c62f434854b6478bab29aab72144
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-12-29 13:14:21 +0100

    Add method for fetching prices

--- internal/client/coingecko_client.go
+++ internal/client/coingecko_client.go
@@ -1,10 +1,15 @@
 package client
 
 import (
+	"context"
+	"fmt"
+	"github.com/shopspring/decimal"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	proto "order_service/generated/coins"
 	"order_service/internal/config"
+	"order_service/internal/logger"
+	"order_service/internal/types"
 	"sync"
 )
 
@@ -42,6 +47,46 @@ func GetCoinGeckoClient() (*CoinGeckoClient, error) {
 		},
 	)
 	return coinGeckoClientInstance, err
+}
+
+func (c *CoinGeckoClient) GetAllCoinsPrice() ([]*types.CoinPrice, error) {
+	conf := config.GetConfig()
+	log := logger.GetLogger()
+	ctx, cancel := context.WithTimeout(context.Background(), conf.CoingeckoServiceTimeout)
+	defer cancel() // Ensure the context is canceled to avoid resource leaks
+
+	res, err := c.client.GetAllCoinsPrices(ctx, &proto.AllCoinsPricesRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	coinPrices := make([]*types.CoinPrice, 0)
+	for coinID, coinData := range res.Data.AsMap() {
+		coinMap, ok := coinData.(map[string]interface{})
+		if !ok {
+			log.Error().Msgf("invalid coin data for %s", coinID)
+			continue
+		}
+
+		prices := make(map[string]decimal.Decimal)
+		for currency, value := range coinMap {
+			decimalValue, err := decimal.NewFromString(fmt.Sprintf("%v", value))
+			if err != nil {
+				log.Error().Msgf("invalid price for %s in %s: %v", coinID, currency, err)
+				continue
+			}
+			prices[currency] = decimalValue
+		}
+
+		coinPrices = append(
+			coinPrices, &types.CoinPrice{
+				CoinId: coinID,
+				Prices: prices,
+			},
+		)
+	}
+
+	return coinPrices, nil
 }
 
 func (c *CoinGeckoClient) Close() error {
