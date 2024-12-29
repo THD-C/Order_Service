724d147e2816560caf1768d5e094283837e52769
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-12-29 14:33:48 +0100

    Add pending orders

--- internal/server/order.go
+++ internal/server/order.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	proto "order_service/generated/order"
 	"order_service/internal/service/order_service"
+	"time"
 )
 
 const (
@@ -13,14 +14,21 @@ const (
 
 type OrderServer struct {
 	proto.UnimplementedOrderServer
-	buyOrderService  order_service.OrderService
-	sellOrderService order_service.OrderService
+	buyOrderService        order_service.OrderService
+	sellOrderService       order_service.OrderService
+	pendingBuyOrderService *order_service.PendingBuyOrderService
 }
 
 func NewOrderServer() *OrderServer {
+	buyService := &order_service.BuyOrderService{}
+	sellService := &order_service.SellOrderService{}
+	pendingBuyService := order_service.NewPendingBuyOrderService(buyService, sellService)
+	go pendingBuyService.CheckAndExecuteOrders(context.Background(), time.Second*60)
+
 	return &OrderServer{
-		buyOrderService:  &order_service.BuyOrderService{},
-		sellOrderService: &order_service.SellOrderService{},
+		buyOrderService:        buyService,
+		sellOrderService:       sellService,
+		pendingBuyOrderService: pendingBuyService,
 	}
 }
 
@@ -28,12 +36,17 @@ func (s *OrderServer) CreateOrder(
 	ctx context.Context,
 	req *proto.OrderDetails,
 ) (*proto.OrderDetails, error) {
-	switch req.Side {
-	case proto.OrderSide_ORDER_SIDE_BUY:
-		return s.buyOrderService.CreateOrder(ctx, req)
-	case proto.OrderSide_ORDER_SIDE_SELL:
-		return s.sellOrderService.CreateOrder(ctx, req)
-	default:
-		return nil, fmt.Errorf(UnsupportedOrderSide)
+	if req.Type == proto.OrderType_ORDER_TYPE_PENDING {
+		return s.pendingBuyOrderService.CreateOrder(ctx, req)
 	}
+
+	if req.Side == proto.OrderSide_ORDER_SIDE_BUY {
+		return s.buyOrderService.CreateOrder(ctx, req)
+	}
+
+	if req.Side == proto.OrderSide_ORDER_SIDE_SELL {
+		return s.sellOrderService.CreateOrder(ctx, req)
+	}
+
+	return nil, fmt.Errorf(UnsupportedOrderSide)
 }
--- internal/service/order_service/pending_buy_order_service.go
+++ internal/service/order_service/pending_buy_order_service.go
@@ -2,57 +2,67 @@ package order_service
 
 import (
 	"context"
-	"github.com/rs/zerolog/log"
 	proto "order_service/generated/order"
 	"order_service/internal/bussiness_errors"
 	"order_service/internal/cache"
+	"order_service/internal/logger"
 	"order_service/internal/types"
 	"time"
 )
 
 type PendingBuyOrderService struct {
-	orderCache   *cache.OrderCache
-	priceCache   *cache.PriceCache
-	orderService OrderService
+	orderCache       *cache.OrderCache
+	priceCache       *cache.PriceCache
+	buyOrderService  OrderService
+	sellOrderService OrderService
 }
 
 func NewPendingBuyOrderService(
-	priceCache *cache.PriceCache,
-	orderService OrderService,
+	buyOrderService OrderService,
+	sellOrderService OrderService,
 ) *PendingBuyOrderService {
 	return &PendingBuyOrderService{
-		orderCache:   cache.NewOrderCache(),
-		priceCache:   priceCache,
-		orderService: orderService,
+		orderCache:       cache.GetOrderCache(),
+		priceCache:       cache.GetPriceCache(),
+		buyOrderService:  buyOrderService,
+		sellOrderService: sellOrderService,
 	}
 }
 
 func (s *PendingBuyOrderService) CreateOrder(
 	_ context.Context,
-	order *types.Order,
-	fiatCurrency, cryptoCurrency string,
-) error {
-	_, err := s.orderCache.Get(order.ID)
+	order *proto.OrderDetails,
+) (*proto.OrderDetails, error) {
+	var myOrder types.Order
+	if err := myOrder.FromProto(order); err != nil {
+		order.Status = proto.OrderStatus_ORDER_STATUS_REJECTED
+		return order, err
+	}
+
+	_, err := s.orderCache.Get(myOrder.ID)
 	if err == nil {
-		return bussiness_errors.NewCustomError(
+		return order, bussiness_errors.NewCustomError(
 			bussiness_errors.ErrOrderAlreadyExists,
 			bussiness_errors.MsgOrderAlreadyExists,
 		)
 	}
 
-	// TODO: Check if price changed meantime
+	fiatWallet, _ := cache.FetchWallet(myOrder.FiatWalletID)
+	cryptoWallet, _ := cache.FetchWallet(myOrder.CryptoWalletID)
 
 	pendingOrder := &types.PendingOrder{
-		Order:          order,
-		FiatCurrency:   fiatCurrency,
-		CryptoCurrency: cryptoCurrency,
+		Order:          &myOrder,
+		FiatCurrency:   fiatWallet.Currency,
+		CryptoCurrency: cryptoWallet.Currency,
 	}
 	err = s.orderCache.Add(pendingOrder)
 	if err != nil {
-		return err
+		order.Status = proto.OrderStatus_ORDER_STATUS_REJECTED
+		return order, err
 	}
 
-	return nil
+	order.Status = proto.OrderStatus_ORDER_STATUS_ACCEPTED
+	return order, nil
 }
 
 func (s *PendingBuyOrderService) UpdateOrder(
@@ -106,7 +116,7 @@ func (s *PendingBuyOrderService) CheckAndExecuteOrders(
 	for {
 		select {
 		case <-ticker.C:
-			// s.executePendingOrders(ctx)
+			s.executePendingOrders(ctx)
 		case <-ctx.Done():
 			return
 		}
@@ -114,7 +124,13 @@ func (s *PendingBuyOrderService) CheckAndExecuteOrders(
 }
 
 func (s *PendingBuyOrderService) executePendingOrders(ctx context.Context) {
+	s.executePendingBuyOrders(ctx)
+	s.executePendingSellOrders(ctx)
+}
+
+func (s *PendingBuyOrderService) executePendingBuyOrders(ctx context.Context) {
 	var err error
+	log := logger.GetLogger()
 	orders, err := s.orderCache.GetAll()
 	if err != nil {
 		return
@@ -125,12 +141,53 @@ func (s *PendingBuyOrderService) executePendingOrders(ctx context.Context) {
 			continue
 		}
 
-		price, exists := s.priceCache.GetPrice(pendingOrder.CryptoCurrency)
+		priceCoin, exists := s.priceCache.GetPrice(pendingOrder.CryptoCurrency)
 		if !exists {
+			log.Error().Msgf("Could not find prices for %s", pendingOrder.CryptoCurrency)
 			continue
 		}
 
+		price := priceCoin.Prices[pendingOrder.FiatCurrency]
+
 		if price.LessThanOrEqual(pendingOrder.Order.Price) {
+			pendingOrder.Order.Price = price
+			err = s.orderService.ExecuteOrder(ctx, pendingOrder.Order)
+			if err != nil {
+				log.Error().Err(err).Msg("Failed to execute order")
+				continue
+			}
+
+			err = s.orderCache.Delete(pendingOrder.Order.ID)
+			if err != nil {
+				log.Error().Err(err).Msg("Failed to delete order from cache")
+			}
+		}
+	}
+}
+
+func (s *PendingBuyOrderService) executePendingSellOrders(ctx context.Context) {
+	var err error
+	log := logger.GetLogger()
+	orders, err := s.orderCache.GetAll()
+	if err != nil {
+		return
+	}
+
+	for _, pendingOrder := range orders {
+		if pendingOrder.Order.Side != proto.OrderSide_ORDER_SIDE_SELL {
+			continue
+		}
+
+		priceCoin, exists := s.priceCache.GetPrice(pendingOrder.CryptoCurrency)
+		if !exists {
+			log.Error().Msgf("Could not find prices for %s", pendingOrder.CryptoCurrency)
+			continue
+		}
+
+		price := priceCoin.Prices[pendingOrder.FiatCurrency]
+
+		if price.GreaterThanOrEqual(pendingOrder.Order.Price) {
+			pendingOrder.Order.Price = price
 			err = s.orderService.ExecuteOrder(ctx, pendingOrder.Order)
 			if err != nil {
 				log.Error().Err(err).Msg("Failed to execute order")
