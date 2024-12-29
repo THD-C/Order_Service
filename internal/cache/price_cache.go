5fd61f9de4f4a530070f974820db4a72059f3533
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2024-12-29 13:19:49 +0100

    Update GetPrice method

--- internal/cache/price_cache.go
+++ internal/cache/price_cache.go
@@ -3,6 +3,8 @@ package cache
 import (
 	"context"
 	"github.com/shopspring/decimal"
+	"order_service/internal/client"
+	"order_service/internal/logger"
 	"order_service/internal/types"
 	"sync"
 	"time"
@@ -30,31 +32,37 @@ func GetPriceCache() *PriceCache {
 	return priceCacheInstance
 }
 
-func (pc *PriceCache) GetPrice(symbol string) (decimal.Decimal, bool) {
+func (pc *PriceCache) GetPrice(symbol string) (*types.CoinPrice, bool) {
 	value, exists := pc.prices.Load(symbol)
 	if !exists {
-		return decimal.Decimal{}, false
+		return &types.CoinPrice{}, false
 	}
-	price, ok := value.(decimal.Decimal)
+	prices, ok := value.(map[string]decimal.Decimal)
 	if !ok {
-		return decimal.Decimal{}, false
+		return &types.CoinPrice{}, false
 	}
-	return price, true
+	priceCoin := types.CoinPrice{
+		CoinId: symbol,
+		Prices: prices,
+	}
+	return &priceCoin, true
 }
 
 func (pc *PriceCache) UpdatePrices(ctx context.Context, interval time.Duration) {
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
+	log := logger.GetLogger()
 
 	for {
 		select {
 		case <-ticker.C:
 			prices, err := fetchPrices()
 			if err != nil {
+				log.Error().Err(err).Msg("Failed to fetch prices")
 				continue
 			}
-			for symbol, price := range prices {
-				pc.prices.Store(symbol, price)
+			for _, price := range prices {
+				pc.prices.Store(price.CoinId, price.Prices)
 			}
 		case <-ctx.Done():
 			return
@@ -63,10 +71,16 @@ func (pc *PriceCache) UpdatePrices(ctx context.Context, interval time.Duration) 
 }
 
 func fetchPrices() ([]*types.CoinPrice, error) {
-	// coinGeckoClient, err := client.GetCoinGeckoClient()
-	//
-	// if err != nil {
-	// 	return nil, err
-	// }
-	return nil, nil
+	coinGeckoClient, err := client.GetCoinGeckoClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	prices, err := coinGeckoClient.GetAllCoinsPrice()
+	if err != nil {
+		return nil, err
+	}
+
+	return prices, nil
 }
