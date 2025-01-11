b092d33184462a32663c0c09ba7524688796504b
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2025-01-11 20:37:23 +0100

    Add type pending in fetching

--- internal/client/dbmanager_client.go
+++ internal/client/dbmanager_client.go
@@ -82,7 +82,10 @@ func (c *DBManagerClient) FetchAllPendingOrders() ([]*types.Order, error) {
 
 	ordersList, err := c.orders.GetOrders(
 		context.Background(),
-		&order.OrderFilter{Status: order.OrderStatus_ORDER_STATUS_PENDING},
+		&order.OrderFilter{
+			Type:   order.OrderType_ORDER_TYPE_PENDING,
+			Status: order.OrderStatus_ORDER_STATUS_PENDING,
+		},
 	)
 	if err != nil {
 		return nil, err
